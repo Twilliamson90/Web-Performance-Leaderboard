6d090306f100768ad5a35770e24781cc48b16bee
Travis Williamson <Twilliamson90@gmail.com>
2018-08-08 21:55:06 -0400

    Added responsive styling

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -4,7 +4,7 @@ class BoardCard extends React.Component {
   render() {
     const { name } = this.props.details;
     return (
-      <li className="uk-width-1-4">
+      <li className="uk-width-medium-1-4">
         <a
           href="/board/US-News-National-University-XPS9kFda"
           className="board-card"
